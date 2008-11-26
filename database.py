afba7fb21da72f16e0948d9aa9ed33808ed5bee6
Anthony Theocharis <anthonyt@uvic.ca>
2008-11-26 01:26:33 -0800

    Rework database into classes.

--- database.py
+++ database.py
@@ -5,50 +5,6 @@ from sqlalchemy.orm import relation
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('mysql://root:@localhost/3ad', echo=True)
-
-metadata = MetaData()
-
-plugins_table = Table(
-	'plugins',
-	metadata,
-	Column('id', Integer, primary_key=True),
-	Column('name', String(255)),
-	Column('modulename', String(255))
-)
-
-output_table = Table(
-	'output',
-	metadata,
-	Column('id', Integer, primary_key=True),
-	Column('plugin_id', Integer, ForeignKey('plugins.id')),
-	Column('audiofile_id', Integer, ForeignKey('audiofiles.id')),
-	Column('vector', PickleType)
-)
-
-audiofiles_table = Table(
-	'audiofiles',
-	metadata,
-	Column('id', Integer, primary_key=True),
-	Column('filename', String(255))
-)
-
-tags_table = Table(
-	'tags',
-	metadata,
-	Column('id', Integer, primary_key=True),
-	Column('name', String(255))
-)
-
-audiofiles_tags = Table(
-	'audiofiles_tags',
-	metadata,
-	Column('audiofile_id', Integer, ForeignKey('audiofiles.id')),
-	Column('tag_id', Integer, ForeignKey('tags.id'))
-)
-
-
-
 class Plugin(object):
 	def __init__(self, name, modulename):
 		self.name = name
@@ -61,6 +17,7 @@ class Plugin(object):
 	def createData(inputFile):
 		return self.module.createData(inputFile)
 
+
 class PluginOutput(object):
 	def __init__(self, vector):
 		self.vector = vector
@@ -68,12 +25,14 @@ class PluginOutput(object):
 	def __repr__(self):
 		return "<PluginOutput('%s')>" % (self.vector)
 
+
 class AudioFile(object):
 	def __init__(self, filename):
 		self.filename = filename
 
 	def __repr__(self):
 		return "<AudioFile('%s')>" % (self.filename)
+
 
 class Tag(object):
 	def __init__(self, name):
@@ -83,37 +42,91 @@ class Tag(object):
 		return "<Tag('%s')>" % (self.name)
 
 
-mapper(
-	Plugin,
-	plugins_table
-)
+class Database(object):
+	__shared_state = {'session': None}
 
-mapper(
-	PluginOutput,
-	output_table,
-	properties = {
-		'plugin': relation(Plugin, backref='outputs'),
-		'file': relation(AudioFile, backref='outputs')
-	}
-)
+	def create_metadata(self):
+		metadata = MetaData()
 
-mapper(
-	AudioFile,
-	audiofiles_table,
-	properties = {
-		'tags': relation(Tag, secondary=audiofiles_tags)
-	}
-)
+		self.plugins_table = Table(
+			'plugins',
+			metadata,
+			Column('id', Integer, primary_key=True),
+			Column('name', String(255)),
+			Column('modulename', String(255))
+		)
 
-mapper(
-	Tag,
-	tags_table,
-	properties = {
-		'files': relation(AudioFile, secondary=audiofiles_tags)
-	}
-)
+		self.output_table = Table(
+			'output',
+			metadata,
+			Column('id', Integer, primary_key=True),
+			Column('plugin_id', Integer, ForeignKey('plugins.id')),
+			Column('audiofile_id', Integer, ForeignKey('audiofiles.id')),
+			Column('vector', PickleType)
+		)
 
-Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
-session = Session()
+		self.audiofiles_table = Table(
+			'audiofiles',
+			metadata,
+			Column('id', Integer, primary_key=True),
+			Column('filename', String(255))
+		)
 
+		self.tags_table = Table(
+			'tags',
+			metadata,
+			Column('id', Integer, primary_key=True),
+			Column('name', String(255))
+		)
+
+		self.audiofiles_tags = Table(
+			'audiofiles_tags',
+			metadata,
+			Column('audiofile_id', Integer, ForeignKey('audiofiles.id')),
+			Column('tag_id', Integer, ForeignKey('tags.id'))
+		)
+
+
+	def create_mappings(self):
+		mapper(
+			Plugin,
+			self.plugins_table
+		)
+
+		mapper(
+			PluginOutput,
+			self.output_table,
+			properties = {
+				'plugin': relation(Plugin, backref='outputs'),
+				'file': relation(AudioFile, backref='outputs')
+			}
+		)
+
+		mapper(
+			AudioFile,
+			self.audiofiles_table,
+			properties = {
+				'tags': relation(Tag, secondary=self.audiofiles_tags)
+			}
+		)
+
+		mapper(
+			Tag,
+			self.tags_table,
+			properties = {
+				'files': relation(AudioFile, secondary=self.audiofiles_tags)
+			}
+		)
+
+	def __init__(self):
+		self.__dict__ = self.__shared_state
+
+		if self.session == None:
+			self.engine = create_engine('mysql://root:@localhost/3ad', echo=True)
+
+			self.create_metadata();
+			self.create_mappings();
+
+			Session = sessionmaker(bind=self.engine, autoflush=True, transactional=True)
+			self.session = Session()
 
