1c0076ce0a6d9c6e74bfdf2a475c096ff972a466
Anthony Theocharis <anthonyt@uvic.ca>
2008-12-03 14:52:48 -0800

    Simplify the init_database script to add files/tags exclusively from the main input file.

--- init_database.py
+++ init_database.py
@@ -6,29 +6,20 @@ db = Database();
 # create the table
 db.createTables();
 
-# Populate it with our initial data.
-# First, some tags
-tags = [
-	Tag("strings"),
-	Tag("cello")
-]
-
-# Save all of the tags that don't already exist in the database.
-for tag in tags:
-	if db.session.query(Tag).filter_by(name=tag.name).count() < 1:
-		db.saveObject(tag)
-
-# Create an audio file object for each file in the audio directory
+# Create an audio file object for each file in the listings file
+# (Also dynamically create tags to represent each tag in the file)
 input = open("files.txt", 'r')
 for line in input:
 	line = line.split(',')
 	fname = line[0]
 	if db.session.query(AudioFile).filter_by(filename=fname).count() < 1:
+		# If this filename is not already existing in the database...
 		f = AudioFile(fname)
 		tags = filter(None, line[1].strip().split(' '))
 		for tag in tags:
 			tags = db.session.query(Tag).filter_by(name=tag)
 			if tags.count() < 1:
+				# If this tag is not already existing in the database...
 				t = Tag(tag)
 				db.saveObject(t)
 			else:
@@ -36,13 +27,7 @@ for line in input:
 			f.tags.append(t)
 		db.saveObject(f)
 
-#files = [AudioFile('audio/'+f) for f in os.listdir('./audio/')]
-
-#for file in files:
-#	if db.session.query(AudioFile).filter_by(filename=file.filename).count() < 1:
-#		db.saveObject(file)
-
-# Third, set up the default plugins
+# Set up the default plugins
 plugins = [
 	Plugin('charlotte', 'plugins.charlotte')
 ]
