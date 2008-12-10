735f8cd3fe0f3d8d7feeb52d7bbcf997e59b6717
Anthony Theocharis <anthonyt@uvic.ca>
2008-12-10 03:57:25 -0800

    Simplify bextract.py
    remove weka output, extraneous print statements, and annotators

--- plugins/bextract_plugin.py
+++ plugins/bextract_plugin.py
@@ -16,33 +16,17 @@ def createVector(filename):
 	fnet.addMarSystem(mng.create("SoundFileSource", "src"))
 	fnet.addMarSystem(mng.create("TimbreFeatures", "featExtractor"))
 	fnet.addMarSystem(mng.create("TextureStats", "tStats"))
-	fnet.addMarSystem(mng.create("Annotator", "annotator"))
-	fnet.addMarSystem(mng.create("WekaSink", "wsink"))
-
-	# link the controls to coordinate things
-	fnet.linkControl("mrs_string/filename", "SoundFileSource/src/mrs_string/filename")
-	fnet.linkControl("mrs_bool/notEmpty", "SoundFileSource/src/mrs_bool/notEmpty")
-	fnet.linkControl("WekaSink/wsink/mrs_string/currentlyPlaying","SoundFileSource/src/mrs_string/currentlyPlaying")
-	fnet.linkControl("Annotator/annotator/mrs_natural/label", "SoundFileSource/src/mrs_natural/currentLabel")
-	fnet.linkControl("SoundFileSource/src/mrs_natural/nLabels", "WekaSink/wsink/mrs_natural/nLabels")
 
 	# update controls to setup things
 	fnet.updControl("TimbreFeatures/featExtractor/mrs_string/disableTDChild", marsyas.MarControlPtr.from_string("all"))
 	fnet.updControl("TimbreFeatures/featExtractor/mrs_string/disableLPCChild", marsyas.MarControlPtr.from_string("all"))
 	fnet.updControl("TimbreFeatures/featExtractor/mrs_string/disableSPChild", marsyas.MarControlPtr.from_string("all"))
 	fnet.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild", marsyas.MarControlPtr.from_string("MFCC/mfcc"))
-	fnet.updControl("mrs_string/filename", marsyas.MarControlPtr.from_string(filename))
-	fnet.updControl("WekaSink/wsink/mrs_string/labelNames", fnet.getControl("SoundFileSource/src/mrs_string/labelNames"))
-	fnet.updControl("WekaSink/wsink/mrs_string/filename", marsyas.MarControlPtr.from_string("bextract_python.arff"))
+	fnet.updControl("SoundFileSource/src/mrs_string/filename", marsyas.MarControlPtr.from_string(filename))
 
 	# do the processing extracting MFCC features and writing to weka file
-	previouslyPlaying = ""
 	while fnet.getControl("SoundFileSource/src/mrs_bool/notEmpty").to_bool():
-		currentlyPlaying = fnet.getControl("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
-		if (currentlyPlaying != previouslyPlaying):
-			print "Processing: " +  fnet.getControl("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
 		fnet.tick()
-		previouslyPlaying = currentlyPlaying
 
 	result = fnet.getControl("mrs_realvec/processedData").to_realvec()
 	result.normMaxMin()
