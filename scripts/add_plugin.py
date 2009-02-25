anthonyt/3ad

commit 9cd9ae605fcd8cd9e104c6a390b4b31e1633e105

Rearrange directory structure to make more sense w.r.t python modules vs scripts
creates a module named ad3 for library code
moves command line scripts to their own directory
moves some old shell scripts to the setup directory

scripts/add_plugin.py
@@ -1,11 +1,9 @@
 import sys
-from model import *
-from controller import *
+from ad3.controller import controller
 
 def main(argv=sys.argv):
 	name = argv[1]
 	modulename = argv[2]
-	db = Database()
 
 	try:
 		plugin = controller.add_plugin(name, modulename)

scripts/generate_tags.py
@@ -1,7 +1,7 @@
 import os
 import getopt
 import sys
-from controller import *
+from ad3.controller import controller
 
 def main(argv):
 

ad3/__init__.py
@@ -1,0 +1,2 @@
+import sys
+sys.path.append("..")

ad3/controller.py
@@ -2,8 +2,7 @@ import sys
 import getopt
 import os
 from numpy import array, concatenate, divide, mean
-from model import *
-import test
+from ad3.model import Database, AudioFile, Plugin, PluginOutput, Tag
 
 # Global framework variables
 db = Database()
@@ -13,32 +12,32 @@ class controller(object):
 	@staticmethod
 	def regenerate_all_tag_locations():
 		# Update the vector for every tag, based on the new output
-		for tag in db.session.query(Tag):
+		for tag in db.query(Tag):
 			print "Creating vector for", tag
 			tag.updateVector()
 
 	@staticmethod
 	def regenerate_all_plugins(filename=None):
 		# Run every plugin over every file, computing a whole bunch of Output Vectors.
-		plugins = db.session.query(Plugin).order_by(Plugin.name)
+		plugins = db.query(Plugin).order_by(Plugin.name)
 
-		files = db.session.query(AudioFile)
+		files = db.query(AudioFile)
 
 		# For individual files, get the file object, then delete old PluginOutput for that file
 		if filename is not None:
 			files = files.filter_by(filename = filename)
-			audiofile = db.session.query(AudioFile).filter_by(filename=filename).one()
-			for old_output in db.session.query(PluginOutput).filter_by(file=audiofile):
+			audiofile = db.query(AudioFile).filter_by(filename=filename).one()
+			for old_output in db.query(PluginOutput).filter_by(file=audiofile):
 				db.session.delete(old_output)
 
 		if files.count() == 0:
 			print "No files found with the name", filename
 			return
-		
+
 		for plugin in plugins:
 			# delete all of the old output for this plugin.
 			if filename is None:
-				for old_output in db.session.query(PluginOutput).filter_by(plugin=plugin):
+				for old_output in db.query(PluginOutput).filter_by(plugin=plugin):
 					db.session.delete(old_output)
 
 			# iterate over each file, generating new output.
@@ -50,13 +49,13 @@ class controller(object):
 
 	@staticmethod
 	def regenerate_plugin(plugin_name):
-		plugin = db.session.query(Plugin).filter_by(name=plugin_name).all()[0]
+		plugin = db.query(Plugin).filter_by(name=plugin_name).all()[0]
 		# delete all of the old output for this plugin.
-		for old_output in db.session.query(PluginOutput).filter_by(plugin=plugin):
+		for old_output in db.query(PluginOutput).filter_by(plugin=plugin):
 			db.session.delete(old_output)
-	
+
 		# iterate over each file, generating new output.
-		for file in db.session.query(AudioFile):
+		for file in db.query(AudioFile):
 			print "Creating vector for", file, plugin
 			output = plugin.createVector(file);
 
@@ -65,14 +64,14 @@ class controller(object):
 	@staticmethod
 	def generate_tags(filename=None, tolerance=None):
 #		if tolerance is None:
-#			tolerance  = sum([p.findMaxDistanceFromAverage() for p in db.session.query(Plugin)])
-#			tolerance -= sum([p.findMinDistanceFromAverage() for p in db.session.query(Plugin)])
+#			tolerance  = sum([p.findMaxDistanceFromAverage() for p in db.query(Plugin)])
+#			tolerance -= sum([p.findMinDistanceFromAverage() for p in db.query(Plugin)])
 #		else:
-#			tolerance += sum([p.findMinDistanceFromAverage() for p in db.session.query(Plugin)])
+#			tolerance += sum([p.findMinDistanceFromAverage() for p in db.query(Plugin)])
 
 		print "Tolerance:", tolerance
 
-		files = db.session.query(AudioFile)
+		files = db.query(AudioFile)
 		if filename is not None:
 			files = files.filter_by(filename = filename)
 
@@ -84,23 +83,23 @@ class controller(object):
 			print ""
 			for tag in file.tags:
 				print "SEED DATA: ", file, tag
-			for tag in db.session.query(Tag):
+			for tag in db.query(Tag):
 				if file.distanceFromTag(tag) <= tolerance:
 					print "GENERATED: ", file, tag
 
 	@staticmethod
 	def add_file(filename, tagstring=""):
-		if db.session.query(AudioFile).filter_by(filename=filename).count() < 1:
+		if db.query(AudioFile).filter_by(filename=filename).count() < 1:
 			# If this filename is not already existing in the database...
 			f = AudioFile(filename)
 			controller.tag_file(f, tagstring)
 			db.saveObject(f)
-	
+
 	@staticmethod
 	def tag_file(file, tagstring):
 		tags = filter(None, tagstring.strip().split(' '))
 		for tag in tags:
-			tags = db.session.query(Tag).filter_by(name=tag)
+			tags = db.query(Tag).filter_by(name=tag)
 			if tags.count() < 1:
 				# If this tag is not already existing in the database...
 				t = Tag(tag)
@@ -112,7 +111,7 @@ class controller(object):
 
 	@staticmethod
 	def add_plugin(name, modulename):
-		query = db.session.query(Plugin).filter_by(modulename=modulename)
+		query = db.query(Plugin).filter_by(modulename=modulename)
 		if query.count() < 1:
 			plugin = Plugin(name, modulename)
 			db.saveObject(plugin)
@@ -122,7 +121,7 @@ class controller(object):
 	@staticmethod
 	def generate_tags_for_file(filename, tolerance, tagstring=""):
 		# Check to see if this filename already exists in the database
-		query = db.session.query(AudioFile).filter_by(filename=filename)
+		query = db.query(AudioFile).filter_by(filename=filename)
 		if query.count() < 1:
 			# If not already existing in the database, run the add_file script to create a new object
 			file = controller.add_file(filename, tagstring)

ad3/model.py
@@ -103,6 +103,9 @@ class Database(object):
 	def saveObject(self, object):
 		return self.session.save(object)
 
+	def query(self, *entities, **kwargs):
+		return self.session.query(*entities, **kwargs)
+
 	def __create_metadata(self):
 		self.metadata = MetaData()
 

commit 6321e5fe8cbe174973052d69601abb12a9a2344a

fix bugs introduced to scripts in 9cd9ae605fcd8cd9e104c6a390b4b31e1633e105

scripts/add_plugin.py
@@ -8,10 +8,10 @@ def main(argv=sys.argv):
     try:
         plugin = controller.add_plugin(name, modulename)
         print "Created Plugin: ", plugin
-        return 0
+        sys.exit(0)
     except ImportError:
         print "Cannot find module ", modulename
-        return 1
+        sys.exit(1)
 
 if __name__ == "__main__":
     sys.exit(main())

scripts/generate_tags.py
@@ -37,16 +37,16 @@ if __name__ == "__main__":
             filename = None
     except getopt.GetoptError:
         usage()
-        return -1
+        sys.exit(1)
 
     # If no file was passed on the command line, calculate vectors and generate tags for all files currently in the database
     if filename is None:
         controller.generate_tags(None, tolerance)
-        return
+        sys.exit(0)
     else:
         # Get the file name from the command line args and check if it exists
         if not os.path.exists(filename):
             print("ERROR: '%s' No such file exists" % filename)
-            return -1
+            sys.exit(1)
         controller.generate_tags_for_file(filename, tolerance, tags)
 