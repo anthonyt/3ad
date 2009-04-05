85fbad10d5d558df9aa07cb4c15a595c2fb780e5
Anthony Theocharis <anthonyt@uvic.ca>
2009-04-04 20:34:13 -0700

    Add some testing/reporting code to test SVM vs Euclidean vs Gaussian classifiers

--- ad3/learning/euclid.py
+++ ad3/learning/euclid.py
@@ -99,8 +99,11 @@ def test():
     class_names = "first_class,second_class"
     num_classes = 2
 
-    d1 = copy([d for d in data if d[-1] == 0])
+    training_class = 1
+    d1 = copy([d for d in data if d[-1] == training_class])
     d2 = copy(data)
+    for d in d1:
+        d[-1] = 0
     for d in d2:
         d[-1] = 0
 
@@ -111,16 +114,61 @@ def test():
     t_data_vec.transpose()
     p_data_vec.transpose()
 
+    # Get the gaussian guesses. Give a tolerance of 100
     classifier_string = train_gaussian(t_data_vec)
     guesses_g = predict_gaussian(p_data_vec, classifier_string)
+    guesses_g = [g[2] < 100 for g in guesses_g]
     print "Size of classifier string:", len(classifier_string)
 
+    # Get the SVM guesses. 1 == True, -1 == False
     classifier_string = train_svm(t_data_vec)
     guesses_s = predict_svm(p_data_vec, classifier_string)
+    guesses_s = [s[0]==1 for s in guesses_s]
     print "Size of classifier string:", len(classifier_string)
 
+    # Get the euclidean guesses. Give a tolerance of 5.
+    means = mean(d1, axis=0)
+    guesses_e = [euclidean_distance(d, means)<5 for d in d2]
+
+    answers = [[
+                data[i][-1]==training_class,
+                guesses_s[i],
+                guesses_g[i],
+                guesses_e[i]
+               ] for i in range(0, len(d2)) ]
+
+    s1 = [b for b in answers if b[0] is True]
+    total_p = len(s1)
+    svm_tp = float(len([a for a in s1 if a[1] == True]))
+    gau_tp = float(len([a for a in s1 if a[2] == True]))
+    euc_tp = float(len([a for a in s1 if a[3] == True]))
+
+    s2 = [b for b in answers if b[0] is False]
+    total_n = len(s2)
+    svm_tn = float(len([a for a in s2 if a[1] == False]))
+    gau_tn = float(len([a for a in s2 if a[2] == False]))
+    euc_tn = float(len([a for a in s2 if a[3] == False]))
+
+    svm_recall = svm_tp/total_p
+    gau_recall = gau_tp/total_p
+    euc_recall = euc_tp/total_p
+    svm_spec = svm_tn/total_n
+    gau_spec = gau_tn/total_n
+    euc_spec = euc_tn/total_n
+
+    """
     for i in range(0, len(d2)):
-        print "Actual:", data[i][-1]==0, "  SVM:", int(guesses_s[i][0])==1, "  Gaussian:", guesses_g[i][2]<100
+        print "Actual:", answers[i][0], \
+            "  SVM:", answers[i][1], \
+            "  Gaussian:", answers[i][2], \
+            "  Euclidean:", answers[i][3]
+    """
+
+    # Print a report
+    print "SVM Recall:      ", svm_recall, "  SVM Specificity:      ", svm_spec
+    print "Gaussian Recall: ", gau_recall, "  Gaussian Specificity: ", gau_spec
+    print "Euclidean Recall:", euc_recall, "  Euclidean Specificity:", euc_spec
+
 
 
 def train_gaussian(data):
